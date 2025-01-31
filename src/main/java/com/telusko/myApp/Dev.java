424a8311b3688581823ecc617b3b62f040afbddf
akhilaakula <akhilaadp@gmail.com>
2025-01-30 22:19:59 -0800

    Dependency Injection

---
+++ src/main/java/com/telusko/myApp/Dev.java
@@ -0,0 +1,10 @@
+package com.telusko.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("I am going to build an Awesome project!!!");
+    }
+}
